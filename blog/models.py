b2b3b05dc5a4ad68d8563ea1aaa0fbe004e38b8e
Naiara Pacheco <naiarapachecosantos@gmail.com>
2025-03-04 18:29:38 -0300

    Adiciona campo category ao modelo Post e cria migração para atualizar o banco de dados

--- blog/models.py
+++ blog/models.py
@@ -7,13 +7,23 @@ class Category(models.Model):
     class Meta:
         ordering = ('title',)
         verbose_name_plural = 'Categories'
+
+    def __str__ (self):
+        return self.title
         
 class Post(models.Model):
+    category = models.ForeignKey(Category, related_name='posts', on_delete=models.CASCADE, default=1)
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     intro = models.TextField()
     body = models.TextField()  
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created_at', )
+
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
@@ -24,3 +34,5 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.name
+    
+    
