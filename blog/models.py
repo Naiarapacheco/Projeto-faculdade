a5a29ee185789790e58be0ff78551c227a2d49a6
Naiara Pacheco <naiarapachecosantos@gmail.com>
2025-03-04 15:05:21 -0300

    Criação da classe Comment

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,12 @@ class Post(models.Model):
     intro = models.TextField()
     body = models.TextField()  
     created_at = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
