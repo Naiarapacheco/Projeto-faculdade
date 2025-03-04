892d009f4f3187bd3a39f5dbaaea0abd46e4dcd1
Naiara Pacheco <naiarapachecosantos@gmail.com>
2025-03-04 15:37:03 -0300

    Adicionando novo campo email para a classe Comment

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,7 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
+    email = models.EmailField(default="email@exemplo.com")
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
