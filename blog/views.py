58f0fa43e8bebe38f87d8c9b73fdc1c86ae15e8e
Naiara Pacheco <naiarapachecosantos@gmail.com>
2025-03-04 16:15:15 -0300

    Criação da view detail para exibir posts e permitir comentários

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
-# Create your views here.
+from .forms import CommentForm
+from .models import Post, Comment
+
+def detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    form = CommentForm()
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return redirect('details', slug=slug)
+        else:
+            form = CommentForm()
+
+    return render(request,'blog/detail.html', {'post': post, 'form': form})
