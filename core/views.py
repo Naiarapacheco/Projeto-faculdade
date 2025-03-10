f7a9b31a7cee948f785b3efe03f3696aaa5f245f
Naiara Pacheco <naiarapachecosantos@gmail.com>
2025-03-09 21:13:04 -0300

    Exibindo apenas posts ativos na página inicial

--- core/views.py
+++ core/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from blog.models import Post
 
 def home(request):
-    posts = Post.objects.all()
+    posts = Post.objects.filter(status=Post.ACTIVE)
     return render(request, 'core/home.html', {'posts':posts})
