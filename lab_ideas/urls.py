3349a0e22b46bfa54893b352a4ff35799dd21bb0
Naiara Pacheco <naiarapachecosantos@gmail.com>
2025-03-09 21:14:18 -0300

    Adicionando suporte a arquivos estáticos e alterando nome de 'detail' para 'blog

--- lab_ideas/urls.py
+++ lab_ideas/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
     path('', include('core.urls')),
-    path('detail/', include('blog.urls')),
+    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
